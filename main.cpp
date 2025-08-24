b771bdaae1309c5974ae5972b48013a8a022a35f
Alfredo Garcia <95046266+AldG99@users.noreply.github.com>
2025-08-23 19:27:19 -0600

    Primer commit - Hola Mundo en C++

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Librería para entrada y salida
+
+using namespace std; // Para usar cout sin std::
+
+int main() {
+    cout << "Hola Mundo" << endl; // Imprime en pantalla
+    return 0; // Fin del programa
+}
